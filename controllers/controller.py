1341503ff9c7aa443ca586e6421deb3786d248f3
user <user@users-MacBook-Pro.local>
2022-11-04 16:00:00 +0000

    :white_check_mark: Add and Delete Book Functionality Added

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,18 @@ from models.book import Book
 @app.route('/books')
 def index():
     return render_template('index.html', title='Home', books=books)
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    title = request.form['title']
+    author = request.form['author']
+    genre = request.form['genre']
+    checked_out = True if 'checked_out' in request.form else False
+    new_book = Book(title, author, genre, checked_out)
+    add_new_book(new_book)
+    return render_template('index.html', title='Home', books=books)
+
+@app.route('/books/delete/<index>', methods=['POST'])
+def delete_event(index):
+    remove_book(int(index))
+    return redirect('/books')
